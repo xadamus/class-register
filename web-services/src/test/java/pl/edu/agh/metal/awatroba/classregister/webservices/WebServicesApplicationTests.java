c611e724705f1d3f5d5f4dccb2097a84a3c35543
Adam Wątroba <adam.j.watroba@gmail.com>
2019-05-11 00:54:35 +0200

    init

---
+++ web-services/src/test/java/pl/edu/agh/metal/awatroba/classregister/webservices/WebServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.metal.awatroba.classregister.webservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebServicesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
